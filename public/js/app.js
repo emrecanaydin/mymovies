1cdc9a71498888e4bf479a2194bcba67f5986b57
emrecanaydin <mrcnaydn@gmail.com>
2016-12-26 12:23:43 +0300

    UX, Mobile Optimization

--- public/js/app.js
+++ public/js/app.js
@@ -23,16 +23,31 @@ mymovies.config(['$routeProvider',
 		templateUrl:'templates/404.html',
 		controller: '404'
 	});
-		
 }]);
 
 mymovies.controller('mainCtrl', function($scope, SiteServices){
+	
+	$scope.authList = [
+		{
+		  modelName:"createdAuthValue.sumList",
+		  modelText: "Sistem Özeti Okuma",
+		  status   : false
+		}
+	];
 		
 	$scope.Helpers = SiteServices;
+	
+	$scope.Helpers.Site.deviceSensitivities();
+	
+	$scope.$on('$routeChangeStart', function(next, current) { 
+		SiteServices.MobileMenu.close();
+	 });
+	 
 	
 	$scope.goPreviousPage = function() {
 		window.history.back();
 	};
+	
 	$scope.AddWatchList = function(movie){
 		var watchlist = SiteServices.WatchList.get();
 		newmovieitem = {
@@ -45,6 +60,7 @@ mymovies.controller('mainCtrl', function($scope, SiteServices){
 		localStorage.setItem("userwatchlist", JSON.stringify(watchlist));
 		swal({title: "Success!",text: "Movie added your watchlist.",timer: 900,showConfirmButton: false});
 	}
+	
 	$scope.RemoveMovieWatchList = function(movie){
 		var watchlist = SiteServices.WatchList.get();
 		watchlist = _.without(watchlist, _.findWhere(watchlist, {
@@ -53,6 +69,7 @@ mymovies.controller('mainCtrl', function($scope, SiteServices){
 		localStorage.setItem("userwatchlist", JSON.stringify(watchlist));
 		swal({title: "Success!",text: "Movie removed from your watchlist.",timer: 900,showConfirmButton: false});
 	}
+	
 	$scope.CheckWatchList = function(movieId){
 		var isMovieWatchList = false;
 		if( _.findWhere(SiteServices.WatchList.get(), {imdbID: movieId}) ){
@@ -62,17 +79,18 @@ mymovies.controller('mainCtrl', function($scope, SiteServices){
 		}
 		return isMovieWatchList;
 	}
+	
 });
 
 mymovies.controller('homePage', function($scope,$http, SiteServices) {
 	$scope.runSearch = function() {
 		window.location.href = "#!/SearchMovie/" + document.getElementById("txtMovieName").value + "/Page/1";
 	};
-	SiteServices.setPageTitle("Home Page");
+	SiteServices.Site.setPageTitle("Home Page");
 });
 
 mymovies.controller('404', function($scope,$http, SiteServices) {
-	SiteServices.setPageTitle("404 Page Not Found");
+	SiteServices.Site.setPageTitle("404 Page Not Found");
 });
 
 
@@ -86,18 +104,26 @@ mymovies.controller('searchResults', function($scope,$routeParams,$http, SiteSer
 			//return search result
 			$scope.movies = result.data;
 			//crete pager
-			$scope.pagerArray = [];
 			$scope.pagerLength = Math.round($scope.movies.totalResults / 8);
+			$scope.pagerArray = {
+				pagerItems : [],
+				pagerInfo: {
+					prevIndex: Number($scope.index) - 1,
+					nextIndex : Number($scope.index) + 1,
+					isFirstIndex: Number($scope.index) == 1,
+					isLastIndex: Number($scope.index) == $scope.pagerLength
+				}
+			};
 			for (i = 0; i < $scope.pagerLength; i++){
 				var pagerItem = {
 					number: i,
-					isActive: i == $scope.index -1
+					isActive: i == Number($scope.index) -1
 				}
-				$scope.pagerArray.push(pagerItem);
+				$scope.pagerArray.pagerItems.push(pagerItem);
 			}
-			SiteServices.setPageTitle("Search Results: " + $scope.keyword);
+			console.log($scope.pagerArray);
+			SiteServices.Site.setPageTitle("Search Results: " + $scope.keyword);
 			SiteServices.Preloader.close();
-			SiteServices.Site.scrollTop(500);
 		}, function(error) {
 			alert(error);
 		});
@@ -113,7 +139,7 @@ mymovies.controller('movieDetail', function($scope,$routeParams,$http, SiteServi
 	$scope.getMovieDetail = function() {
 		$http.get(SiteServices.Api.getApiUrl + "?i="+$scope.movieid+"&plot=short&r=json").then(function(result) {
 			$scope.movieObject = result.data;
-			SiteServices.setPageTitle($scope.movieObject.Title);
+			SiteServices.Site.setPageTitle($scope.movieObject.Title);
 			SiteServices.Preloader.close();
 		}, function(error) {
 			alert(error);
@@ -126,58 +152,80 @@ mymovies.controller('watchList', function($scope,$http, SiteServices) {
 	$scope.movieList = [];
 	$scope.loadUserWatchList = function () {
 		$scope.movieList = SiteServices.WatchList.get();
-		SiteServices.setPageTitle("My Watchlist");
+		SiteServices.Site.setPageTitle("My Watchlist");
 	};
 	$scope.loadUserWatchList();
 });
 
+
 mymovies.service('SiteServices', function ($window) {
-	this.Site = {
-		scrollTop: function(duration){
-			var scrollStep = -window.scrollY / (duration / 15),
-				scrollInterval = setInterval(function(){
-				if ( window.scrollY != 0 ) {
-					window.scrollBy( 0, scrollStep );
+	SiteServices = { 
+		Site : {
+			scrollTop: function(duration){
+				var scrollStep = -window.scrollY / (duration / 15),
+					scrollInterval = setInterval(function(){
+					if ( window.scrollY != 0 ) {
+						window.scrollBy( 0, scrollStep );
+					}
+					else clearInterval(scrollInterval); 
+				},15);
+			},
+			deviceSensitivities: function(){
+				window.onresize = function(event) {
+					if( window.innerWidth > 767 ){
+						SiteServices.MobileMenu.close();
+					}
+				};
+			},
+			setPageTitle : function (title) {
+				$window.document.title = title + " | " + SiteServices.Site.siteName;
+			},
+			siteName: "Movie Collector"
+		},
+		Api : {
+			getApiUrl:  "http://www.omdbapi.com/"
+		},
+		WatchList : {
+			get:  function(){
+				return localStorage.getItem("userwatchlist") == null ? [] : JSON.parse(localStorage.getItem("userwatchlist"));
+			}
+		},
+		Preloader : {
+			open: function(){
+				document.body.className += " loading";
+			},
+			close: function(){
+				document.body.classList.remove('loading');
+			}
+		},
+		Modal : {
+			open: function(){
+				document.getElementById("modal-overlay").style["display"] = 'block';
+				document.getElementById("modal").style["display"] = 'block';
+			},
+			close: function(){
+				document.getElementById("modal-overlay").style["display"] = "none";
+				document.getElementById("modal").style["display"] = "none";
+			}
+		},
+		MobileMenu : {
+			open: function(){
+				document.body.className += " mobile-menu-opened";
+			},
+			close: function(){
+				document.body.classList.remove('mobile-menu-opened');
+			},
+			run: function(){
+				if (document.body.classList.contains('mobile-menu-opened')){
+					this.close();
 				}
-				else clearInterval(scrollInterval); 
-			},15);
-		},
-		siteName:"Movie Collector"
-	};
-    this.setPageTitle = function (title) {
-		$window.document.title = title + " | " + this.Site.siteName;
-    };
-	this.Api = {
-		getApiUrl:  "http://www.omdbapi.com/"
-	};
-	this.WatchList = {
-		get:  function(){
-			return localStorage.getItem("userwatchlist") == null ? [] : JSON.parse(localStorage.getItem("userwatchlist"));
+				else{
+					this.open();
+				}
+			}
 		}
-	};
-	this.Preloader = {
-		open: function(){
-			document.body.className += " loading";
-		},
-		close: function(){
-			document.body.classList.remove('loading');
-		}
-	};
-	this.Modal = {
-		open: function(){
-			document.getElementById("modal-overlay").style["display"] = 'block';
-			document.getElementById("modal").style["display"] = 'block';
-		},
-		close: function(){
-			document.getElementById("modal-overlay").style["display"] = "none";
-			document.getElementById("modal").style["display"] = "none";
-		}
-	};
-	this.MobileMenu = function(){
-		if (document.body.classList.contains('mobile-menu-opened')) {
-			document.body.classList.remove('mobile-menu-opened');
-		}else{
-			document.body.className += " mobile-menu-opened";
-		}
-	};
+	}
+	
+	return SiteServices;
+		
 });
